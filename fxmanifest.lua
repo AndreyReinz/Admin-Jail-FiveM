ff43cf7fb98c63b3180ef4d615e58990f0d6e70f
Andrey Reinz <110102592+AndreyReinz@users.noreply.github.com>
2025-08-03 15:42:33 +0700

    GarageSystem/

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AndreyReinz'
+description 'SAVE CARS SYSTEM'
+version '2.0.0'
+
+ui_page 'ui.html'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+    'ui.html'
+}
